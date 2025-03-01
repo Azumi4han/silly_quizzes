06a1ef1d3a94a02e9b4019008a25594d77078ae2
azumichan <chifmaster113@gmail.com>
2025-03-01 22:34:17 +0200

    chore: Add fonts for 'RU' language

---
+++ src/ styled.d.ts
@@ -0,0 +1,10 @@
+import "solid-styled-components";
+
+declare module "solid-styled-components" {
+  export interface DefaultTheme {
+    fonts: {
+      primary: string;
+      secondary: string;
+    };
+  }
+}
